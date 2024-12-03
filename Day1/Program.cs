f851fff0f74617983be09c3275ed9244abce09f8
Ignelio <ignas.rodzianskis@gmail.com>
2024-12-03 13:45:23 +0200

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = [3,4,2,1,3,3]; int[] arr2 = [4,3,5,3,9,3]; int[] diff = new int[arr1.Length >= arr2.Length ? arr2.Length : arr1.Length];
+            arr1 = [.. arr1.OrderDescending()];
+            arr2 = [.. arr2.OrderDescending()];
+            for (int i = 0; i < (arr1.Length >= arr2.Length ? arr2.Length : arr1.Length); i++)
+            {
+                diff[i] = Math.Abs(arr1[i] - arr2[i]);
+            }
+            Console.WriteLine(diff.Sum());
+        }
+    }
+}
